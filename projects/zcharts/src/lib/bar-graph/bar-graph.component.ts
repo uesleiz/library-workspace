d8719200109da8821dc36aa851c2c22db217a4ae
uesleidcn <ueslei@ztrax.com.br>
2024-12-21 10:08:44 -0300

    feat(#00): create bar graph component

---
+++ projects/zcharts/src/lib/bar-graph/bar-graph.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zc-bar-graph',
+  standalone: true,
+  imports: [],
+  templateUrl: './bar-graph.component.html',
+  styleUrl: './bar-graph.component.css'
+})
+export class BarGraphComponent {
+
+}
